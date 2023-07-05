655f3eb0d13204d68263f4ccd18d7bf77e221e8f
Thomas Adams <tomad212@gmail.com>
2023-07-05 10:53:24 +0200

    Depreciate it

--- app/dashboard/NoFine.tsx
+++ app/dashboard/NoFine.tsx
@@ -10,11 +10,11 @@ function NoFine({ showAdd }: Props) {
   return (<>
     <div className="m-auto w-fit">
       <h2 className="text-pearl-cyan font-extrabold text-[3rem]">
-        No fine selected
+        No meeting selected
       </h2>
       <Image className="m-auto" src='/icons/empty-box.svg' alt='no fine box' height={250} width={250} />
       <button onClick={() => showAdd(1)} className="btn w-36 mx-auto mt-3 block">
-        New Fine
+        New meeting
       </button>
     </div>
   </>)
